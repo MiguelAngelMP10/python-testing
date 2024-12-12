39bbd61389092aa45abf0290128df6b84caae2fe
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2024-12-12 12:01:18 -0600

    update

--- tests/tests_all_asserts.py
+++ tests/tests_all_asserts.py
@@ -1,5 +1,6 @@
 import unittest
 
+SERVER="server_b"
 
 class AllAssertsTests(unittest.TestCase):
 
@@ -29,3 +30,15 @@ class AllAssertsTests(unittest.TestCase):
             {1, 2, 3},
             {1, 2, 3}
         )
+
+    @unittest.skip("Trabjo en progreso, será habilitada nuevamente")
+    def test_skip(self):
+        self.assertEqual("hola", "chao")
+
+    @unittest.skipIf(SERVER == "server_b", "Saltado porque no estamos en el servidor")
+    def test_skip_if(self):
+        self.assertEqual(100, 100)
+
+    @unittest.expectedFailure
+    def test_expected_failure(self):
+        self.assertEqual(100, 150)
